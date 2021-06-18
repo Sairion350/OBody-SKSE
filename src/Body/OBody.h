1bc33903baf8fa1ff295ca81274ae3969e2a9350
Qudix <17361645+Qudix@users.noreply.github.com>
2021-06-17 20:52:48 -0500

    First pass

--- src/Body/OBody.h
+++ src/Body/OBody.h
@@ -2,111 +2,155 @@
 
 #include "SKEE.h"
 
-using namespace std;
-
-
 namespace Body
 {
-	
+	struct Slider
+	{
+		std::string name;
+		float min;
+		float max;
+	};
+
+	struct SliderSet
+	{
+		std::vector<Slider> sliders;
+	};
+
+	struct ScoreSet
+	{
+		std::string name;
+		float MinScore;	 // 100 = large?
+		float MaxScore;
+	};
+
+	struct BodyslidePreset
+	{
+		std::string name;
+		std::string body;
+		SliderSet sliders;
+		std::vector<ScoreSet> scores;
+	};
+
+	struct PresetDatabase
+	{
+		std::vector<BodyslidePreset> presets;
+	};
+
+	struct BodysliderSliderScore
+	{
+		std::string name;
+		int score;
+	};
+
+	struct BodypartScoreSet
+	{
+		std::vector<BodysliderSliderScore> scores;
+	};
+
+	struct RaceStat
+	{
+		std::string name;
+		std::string bodypart;
+		int value;
+	};
+
+	struct RaceStatDatabase
+	{
+		std::vector<RaceStat> races;
+	};
 
 	class OBody
 	{
-		using VM = RE::BSScript::IVirtualMachine;
 	public:
-
 		static OBody* GetInstance();
 
-		
-
-		bool GameLoaded;
-
 		void SetLoaded(bool a);
-		float GetBreastScore(struct SliderSet sliders, bool max);
+		float GetBreastScore(SliderSet sliders, bool max);
 
-		vector<string> GetFilesInBodyslideDir();
+		std::vector<std::string> GetFilesInBodyslideDir();
 		void GenerateDatabases();
-		bool StringContains(string& str, const char* testcase);
+		bool StringContains(std::string& str, const char* testcase);
 
-		void GenBodyByFile(RE::Actor* act, string path);
+		void GenBodyByFile(RE::Actor* act, std::string path);
 
-		struct SliderSet GenerateClotheSliders(RE::Actor* act);
-		struct Slider BuildDerivativeSlider(RE::Actor* act, string morph, float target);
+		SliderSet GenerateClotheSliders(RE::Actor* act);
+		Slider BuildDerivativeSlider(RE::Actor* act, std::string morph, float target);
 
 		void GenerateActorBody(RE::Actor* act);
-		void SetMorph(RE::Actor* act, string MorphName, float value, string key);
-		void SetMorphByWeight(RE::Actor* act, struct Slider slider, float weight, string key);
+		void SetMorph(RE::Actor* act, std::string MorphName, float value, std::string key);
+		void SetMorphByWeight(RE::Actor* act, Slider slider, float weight, std::string key);
 		void ApplyMorphs(RE::Actor* act);
 		bool SetMorphInterface(SKEE::IBodyMorphInterface* a_morphInt);
-		void ApplySlider(RE::Actor* act, struct Slider slid, float weight, string key);
+		void ApplySlider(RE::Actor* act, Slider slid, float weight, std::string key);
 		float GetWeight(RE::Actor* act);
-		void ApplySliderSet(RE::Actor* act, struct SliderSet sliderset, string key);
-		void ApplyBodyslidePreset(RE::Actor* act, struct BodyslidePreset preset );
+		void ApplySliderSet(RE::Actor* act, SliderSet sliderset, std::string key);
+		void ApplyBodyslidePreset(RE::Actor* act, BodyslidePreset preset );
 
-		pugi::xml_document GetDocFromFile(string pathToFile);
-		struct SliderSet GenerateSlidersetFromDoc(pugi::xml_document& doc);
+		pugi::xml_document GetDocFromFile(std::string pathToFile);
+		SliderSet GenerateSliderSetFromDoc(pugi::xml_document& doc);
 
-		void AddSliderToSet(struct SliderSet& sliderset, struct Slider slider);
-		struct Slider BuildSlider(string name, float min, float max);
-		struct Slider BuildSlider(string name, float value);
-		vector<struct BodyslidePreset> GeneratePresetsFromDoc(pugi::xml_document& doc);
+		void AddSliderToSet(SliderSet& sliderset, Slider slider);
+		Slider BuildSlider(std::string name, float min, float max);
+		Slider BuildSlider(std::string name, float value);
+		std::vector<BodyslidePreset> GeneratePresetsFromDoc(pugi::xml_document& doc);
 		void ProcessActor(RE::Actor* act);
-		float GetMorph(RE::Actor* act, string MorphName);
-		void GenerateFullBodyFromPreset(RE::Actor* act, struct BodyslidePreset preset);
-		struct BodyslidePreset GenerateSinglePresetFromFile(string file);
-		struct BodyslidePreset GeneratePresetFromNode(pugi::xml_node node);
+		float GetMorph(RE::Actor* act, std::string MorphName);
+		void GenerateFullBodyFromPreset(RE::Actor* act, BodyslidePreset preset);
+		BodyslidePreset GenerateSinglePresetFromFile(std::string file);
+		BodyslidePreset GeneratePresetFromNode(pugi::xml_node node);
 
-		void AddPresetToDatabase(struct PresetDatabase& database, struct BodyslidePreset preset);
-		struct BodyslidePreset GetRandomElementOfDatabase(struct PresetDatabase& database);
-		bool IsFemalePreset(struct BodyslidePreset& preset);
-		struct SliderSet GenerateSlidersetFromNode(pugi::xml_node& node, int body);
-		int GetBodyType(string body);
-		void AddSliderToSet(struct SliderSet& sliderset, struct Slider slider, bool inverted);
+		void AddPresetToDatabase(PresetDatabase& database, BodyslidePreset preset);
+		BodyslidePreset GetRandomElementOfDatabase(PresetDatabase& database);
+		bool IsFemalePreset(BodyslidePreset& preset);
+		SliderSet GenerateSlidersetFromNode(pugi::xml_node& node, int body);
+		int GetBodyType(std::string body);
+		void AddSliderToSet(SliderSet& sliderset, Slider slider, bool inverted);
 
-		void AddPresetToDatabase(struct PresetDatabase& database, vector<struct BodyslidePreset> presets);
-		vector<struct BodyslidePreset> GeneratePresetsFromFile(string file);
+		void AddPresetToDatabase(PresetDatabase& database, std::vector<BodyslidePreset> presets);
+		std::vector<BodyslidePreset> GeneratePresetsFromFile(std::string file);
 
-		void PrintSliderSet(struct SliderSet set);
-		void PrintPreset(struct BodyslidePreset preset);
+		void PrintSliderSet(SliderSet set);
+		void PrintPreset(BodyslidePreset preset);
 
 		void ApplyClothePreset(RE::Actor* act);
 		void ProcessActorEquipEvent(RE::Actor* act, bool RemovingBodyArmor);
 
 		bool IsProcessed(RE::Actor* act);
 
-		bool IsClothedSet(string set);
+		bool IsClothedSet(std::string set);
 		bool IsFemale(RE::Actor* act);
 
 		bool IsNaked(RE::Actor* act);
 
 		bool HasActiveClothePreset(RE::Actor* act);
-		vector<RE::BSFixedString> GetPresets(RE::Actor* act);
-		bool SliderSetContainsSlider(struct SliderSet& set, string slidername);
+		std::vector<RE::BSFixedString> GetPresets(RE::Actor* act);
+		bool SliderSetContainsSlider(SliderSet& set, std::string slidername);
 		void RemoveClothePreset(RE::Actor* act);
-		struct BodyslidePreset GetPresetByName(struct PresetDatabase& database, string name);
-		void GenBodyByName(RE::Actor* act, string PresetName);
-		int GetSliderScore(struct BodypartScoreset& scoreset, string slidername);
+		BodyslidePreset GetPresetByName(PresetDatabase& database, std::string name);
+		void GenBodyByName(RE::Actor* act, std::string PresetName);
+		int GetSliderScore(BodypartScoreSet& scoreset, std::string slidername);
 
-		float GetBodypartScore(struct BodypartScoreset& bodypartSet, struct SliderSet sliders, bool max);
-		float GetButtScore(struct SliderSet sliders, bool max);
-		float GetWaistScore(struct SliderSet sliders, bool max);
+		float GetBodypartScore(BodypartScoreSet& bodypartSet, SliderSet sliders, bool max);
+		float GetButtScore(SliderSet sliders, bool max);
+		float GetWaistScore(SliderSet sliders, bool max);
 		void RegisterQuestForEvent(RE::TESQuest* quest);
-		float GetScoreByWeight(struct ScoreSet& score, float& weight);
+		float GetScoreByWeight(ScoreSet& score, float& weight);
 		void SaveScoreToActor(RE::Actor* act, struct ScoreSet& score, float& weight);
 		float RandomFloat(float a, float b);
-		struct SliderSet GenerateRandomNippleSliders();
+		SliderSet GenerateRandomNippleSliders();
 		bool ChanceRoll(int chance);
-		struct SliderSet GenerateRandomGenitalSliders();
+		SliderSet GenerateRandomGenitalSliders();
 
 		void BuildRaceStatDB();
-		struct RaceStat GenerateRaceStatFromNode(pugi::xml_node node);
-		struct PresetDatabase SortPresetDatabaseByRaceStat(struct PresetDatabase& database, struct RaceStat stat);
-		bool comparePresetByBreastScore(const BodyslidePreset &a, const BodyslidePreset &b);
-		bool comparePresetByButtScore(const BodyslidePreset &a, const BodyslidePreset &b);
-		bool comparePresetByWaistScore(const BodyslidePreset &a, const BodyslidePreset &b);
-		bool comparePresetByScore(const BodyslidePreset &a, const BodyslidePreset &b, string scorename);
-		struct ScoreSet GetScoresetFromPresetByName(struct BodyslidePreset& preset, string scorename);
-		struct PresetDatabase SortPresetDatabaseByBodypart(struct PresetDatabase& database, string bodypart);
-		void PrintDatabase(struct PresetDatabase& database);
+		RaceStat GenerateRaceStatFromNode(pugi::xml_node node);
+		PresetDatabase SortPresetDatabaseByRaceStat(PresetDatabase& database, RaceStat stat);
+		bool ComparePresetByBreastScore(const BodyslidePreset &a, const BodyslidePreset &b);
+		bool ComparePresetByButtScore(const BodyslidePreset &a, const BodyslidePreset &b);
+		bool ComparePresetByWaistScore(const BodyslidePreset &a, const BodyslidePreset &b);
+		bool ComparePresetByScore(const BodyslidePreset& a, const BodyslidePreset& b, std::string scorename);
+		ScoreSet GetScoresetFromPresetByName(BodyslidePreset& preset, std::string scorename);
+		PresetDatabase SortPresetDatabaseByBodypart(PresetDatabase& database, std::string bodypart);
+		void PrintDatabase(PresetDatabase& database);
 		int RandomInt(int a, int b);
 		int GetFemaleDatabaseSize();
 		int GetMaleDatabaseSize();
@@ -115,10 +159,26 @@ namespace Body
 		void SetNippleRand(bool a);
 		void SetGenitalRand(bool a);
 
+		RaceStat GetCorrespondingRaceStat(RE::Actor* act);
 
+	public:
+		bool GameLoaded;
+		bool UseRaceStats = true;
 
-		struct RaceStat GetCorrespondingRaceStat(RE::Actor* act);
+		bool set_ORefit = true;
+		bool set_NippleRand = true;
+		bool set_GenitalRand = true;
 
+		std::vector<std::string> oneDefaultSliders;
+
+		RaceStatDatabase RaceStats;
+
+		PresetDatabase FemalePresets;
+		PresetDatabase MalePresets;
+
+		BodypartScoreSet BreastScores;
+		BodypartScoreSet ButtScores;
+		BodypartScoreSet WaistScores;
 
 	private:
 		OBody() = default;
@@ -127,9 +187,5 @@ namespace Body
 		~OBody() = default;
 
 		SKEE::IBodyMorphInterface* morphInt;
-
-
-
-
 	};
 }
