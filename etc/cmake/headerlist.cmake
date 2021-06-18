189b256f9c8083f75b0dd4bcb15e1b423aa2678e
Qudix <17361645+Qudix@users.noreply.github.com>
2021-06-17 20:55:36 -0500

    CMake stuff

--- etc/cmake/headerlist.cmake
+++
@@ -1,9 +0,0 @@
-set(headers ${headers}
-	src/Papyrus.h
-	src/PCH.h
-	src/SKEE.h
-	src/Body/Actors.h
-	src/Body/Morph.h
-	src/Body/MorphProcedural.h
-	src/Body/OBody.h
-)
--- etc/cmake/sourcelist.cmake
+++ etc/cmake/sourcelist.cmake
@@ -1,9 +1,17 @@
-set(sources ${sources}
-	src/main.cpp
-	src/Papyrus.cpp
-	src/PCH.cpp
+set(SOURCES
 	src/Body/Actors.cpp
+	src/Body/Actors.h
 	src/Body/Morph.cpp
+	src/Body/Morph.h
 	src/Body/MorphProcedural.cpp
+	src/Body/MorphProcedural.h
 	src/Body/OBody.cpp
+	src/Body/OBody.h
+	src/Event/Event.h
+	src/PCH.cpp
+	src/PCH.h
+	src/Papyrus.cpp
+	src/Papyrus.h
+	src/SKEE.h
+	src/main.cpp
 )
