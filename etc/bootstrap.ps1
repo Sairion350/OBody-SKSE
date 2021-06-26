cc8631190eb82c876ee416871b39f07991355990
Qudix <17361645+Qudix@users.noreply.github.com>
2021-06-25 19:30:04 -0500

    Better bootstrap Support running through the Windows Terminal (if available)

---
+++ etc/bootstrap.ps1
@@ -0,0 +1,4 @@
+git submodule update --init --recursive
+cmake -B "..\build" -S "..\"
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
